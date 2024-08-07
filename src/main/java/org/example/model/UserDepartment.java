3ffd57c146e3b870b9245836e0283f1b3f4b4529
Olga8I <olga.filonchik@gmail.com>
2024-08-07 22:32:28 +0300

    Сhanges #2

--- src/main/java/org/example/model/UserDepartment.java
+++ src/main/java/org/example/model/UserDepartment.java
@@ -1,4 +1,48 @@
 package org.example.model;
 
+import org.springframework.stereotype.Component;
+
+/**
+ * UserDepartment entity
+ * ManyToMany
+ * User <-> Department
+ */
+@Component
 public class UserDepartment {
+    private Long id;
+    private Long userId;
+    private Long departmentId;
+
+    public UserDepartment() {
+    }
+
+    public UserDepartment(Long id, Long userId, Long departmentId) {
+        this.id = id;
+        this.userId = userId;
+        this.departmentId = departmentId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
 }
